4d2803353193d027cb0d1423258ed75b02cf76ae
rocket-pocket <verdudaniel7@gmail.com>
2018-08-31 10:41:43 +0200

    inicio

---
+++ app3.js
@@ -0,0 +1,16 @@
+const animateArea = document.querySelectorAll(".animate-area");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add("fade");
+    } else {
+      entry.target.classList.remove("fade");
+    }
+  });
+});
+
+animateArea.forEach(item => {
+  observer.observe(item);
+});
+
